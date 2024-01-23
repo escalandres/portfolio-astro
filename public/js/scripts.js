5ba66eafdf4335c7716fda5997a4d20a8879ba2e
Andres Escala <79557629+escalandres@users.noreply.github.com>
2024-01-22 22:01:28 -0600

    modify: transparent navbar background added

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,11 @@
+function showNotification(){
+    document.getElementById("notification").classList.remove("hidden");
+}
+
+function hideNotification(){
+    document.getElementById("notification").classList.add("hidden");
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     // Tu código aquí
     document.getElementById("copy-btn").addEventListener("click", () => {
@@ -16,10 +24,20 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-function showNotification(){
-    document.getElementById("notification").classList.remove("hidden");
-}
+window.addEventListener('scroll', function () {
+    var scrollPosition = window.scrollY;
 
-function hideNotification(){
-    document.getElementById("notification").classList.add("hidden");
-}
+    // Iterar sobre las secciones y aplicar estilos dinámicamente
+    var section = document.getElementById('home');
+    var nav = document.getElementById('navbar');
+
+    var sectionTop = section.offsetTop;
+
+    if (scrollPosition >= sectionTop) {
+        nav.classList.add('backdrop-blur-sm');
+    }else{
+        nav.classList.remove('backdrop-blur-sm');
+    }
+
+});
+
